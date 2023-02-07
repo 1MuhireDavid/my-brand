f6ff8b12c6f1945b817fd8be804f913992453b83
1MuhireDavid <muhiredavid74@gmail.com>
2023-02-07 14:31:52 +0200

    chore(dash): add integration with backend

--- dash/scripts.js
+++ dash/scripts.js
@@ -2,12 +2,13 @@
 
 var form = `<div>
   <div class="form-group">
+  <form>
     <label for="name">Title</label>
     <input type="text" class="form-control" id="title" aria-describedby="emailHelp" placeholder="Enter Blog Title">
   </div>
   <div class="form-group">
     <label for="name">Upload blog image</label>
-    <input type="file" class="form-control" id="pic">
+    <input type="url" class="form-control" id="pic">
   </div>
   <div class="form-group">
     <label >Description</label>
@@ -16,7 +17,8 @@ var form = `<div>
   </div>
 
   <button type="submit" class="btn btn-big" onclick="save()">Save</button>
-</div>`;
+</form>
+  </div>`;
 
 function table() {
     let table = `<table class="table">
@@ -63,25 +65,22 @@ function setData() {
     localStorage.setItem("details", JSON.stringify(details));
 };
 
+//var image
+// blogimage.addEventListener("change", (e)=>{
+//   const img = e.target.files[0];
+//   const reader=new FileReader();
+//   reader.readAsDataURL(img);
 
-var image
-let blogimage = document.getElementById("pic");
-blogimage.addEventListener("change", (e)=>{
-  const img = e.target.files[0];
-  const reader=new FileReader();
-  reader.readAsDataURL(img);
-
-  reader.addEventListener("load",  ()=>{
-    image = reader.result;
-  })
-})
-
+//   reader.addEventListener("load",  ()=>{
+//     image = reader.result;
+//   })
+// })
 
 
 function save() {
     let title = document.getElementById("title");
     let description = document.getElementById("description");
-   
+    let image = document.getElementById("pic");
 
     if (title.value == 0 || description.value == 0) {
         alert("Please enter all the fields");
@@ -90,13 +89,40 @@ function save() {
     let data = {
         title: title.value,
         description: description.value,
-        blogimage: image
+        blogimage: image.value
     };
+  console.log(title.value, description.value, image.value);
+ // const token =
+ //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGMyN2QyODZiMGM4MzZhNzc5ODU3MiIsImlhdCI6MTY3NTc2NTU1NiwiZXhwIjoxNjc1ODUxOTU2fQ.lzJWBpnuH1lKc7LrQSgsUVPV2_u6iAueAx8iv89bTpE";
+  const url = "http://localhost:3000/posts";
+  const body = {
+    title: title.value,
+    description: description.value,
+    imageUrl: image.value,
+  };
+  fetch(url, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
     details.push(data);
     setData();
 
     // console.log(details)
     // console.log(email.value)
+  
+
+
     table();
     title.value = "";
     description.value = "";
